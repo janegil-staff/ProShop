ce3487faf324ad1ab0ec5ea730fa304233b3024b
Jan Egil Staff Øvretveit <janstovr@gmail.com>
2024-11-27 17:28:26 +0100

    Backend Authentication

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,21 +1,20 @@
-const notFound = ({ req, res, next }) => {
+const notFound = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
-  req.status(404);
+  res.status(404);
   next(error);
-}
+};
 
 const errorHandler = (err, req, res, next) => {
   let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   let message = err.message;
 
-  if(err.name === 'CastError' && err.kind === 'ObjectId'){
-    message = `Resource not Found`;
-    statusCode = 404;
-  }
+  // NOTE: checking for invalid ObjectId moved to it's own middleware
+  // See README for further info.
 
   res.status(statusCode).json({
-    message,
-    stack: process.env.NODE_ENV === 'production' ? '' : err.stack });
+    message: message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  });
 };
 
 export { notFound, errorHandler };
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 dotenv.config();
-import productRoutes from './routes/productRoutes.js';
+
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+
 import connectDB from "./config/db.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
@@ -9,10 +13,14 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-
 const app = express();
 
-app.use('/api/products', productRoutes);
+app.use(express.json());
+app.use(express(express.urlencoded({extended: true})));
+app.use(cookieParser());
+
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
